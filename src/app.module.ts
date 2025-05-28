e91936ae0a46ab9022f8f6d53edade42e6c5b7fc
Zuxriddinoff <zuxriddinovoff@gmail.com>
2025-05-28 11:44:29 +0500

    user model

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { SequelizeModule } from '@nestjs/sequelize';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [ConfigModule.forRoot({
@@ -19,6 +20,7 @@ import { SequelizeModule } from '@nestjs/sequelize';
       logging: false,
       models: [ ]
     }),
+    UserModule,
   ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import config from './config';
 
 async function bootstrap() {
-  const PORT: number = Number(process.env.PORT);
+  const PORT:number = config.PORT
   const app = await NestFactory.create(AppModule);
   await app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
