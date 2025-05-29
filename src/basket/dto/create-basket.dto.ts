a6e2d22570ed91b64daed8b61b386a67d7230c3d
isfandiyor <isfandiyor.vohidovv@mail.ru>
2025-05-29 09:05:06 +0500

    Validation for payment

--- src/basket/dto/create-basket.dto.ts
+++ src/basket/dto/create-basket.dto.ts
@@ -1,5 +1,15 @@
+import { IsNotEmpty, IsNumber } from "class-validator";
+
 export class CreateBasketDto {
+    @IsNotEmpty()
+    @IsNumber()
     user_id: number;
+
+    @IsNotEmpty()
+    @IsNumber()
     quantity: number;
+
+    @IsNotEmpty()
+    @IsNumber()
     product_id: number;
 }
